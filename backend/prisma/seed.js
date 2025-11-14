e4f9326f950fc2e2de3335bb0542170db4476186
floharmenzon <floharmenzon@gmail.com>
2025-11-14 13:03:58 +0100

    Fixed ticket status

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -11,9 +11,9 @@ async function main() {
             date: new Date("2026-01-10T17:00:00Z"),
             tickets: {
                 create: [
-                    { name: "Early Bird", price: 5, max: 50, code: "EARLY" },
-                    { name: "Standard", price: 7.5, max: 600, code: "STANDARD" },
-                    { name: "Latecomer", price: 10, max: 100, code: "LATE" }
+                    { name: "Early Bird", price: 5, max: 50, sold: 50, code: "EARLY", status: "sold-out" },
+                    { name: "Standard", price: 7.5, max: 600, sold: 0, code: "STANDARD", status: "available" },
+                    { name: "Latecomer", price: 10, max: 100, sold: 0, code: "LATE", status: "coming-soon" }
                 ]
             }
         }
@@ -28,9 +28,9 @@ async function main() {
             date: new Date("2026-02-14T17:00:00Z"),
             tickets: {
                 create: [
-                    { name: "Early Bird", price: 6, max: 50, code: "EARLY" },
-                    { name: "Standard", price: 8.18, max: 600, code: "STANDARD" },
-                    { name: "Latecomer", price: 12, max: 100, code: "LATE" }
+                    { name: "Early Bird", price: 6, max: 50, sold: 0, code: "EARLY", status: "available" },
+                    { name: "Standard", price: 8.18, max: 600, sold: 600, code: "STANDARD", status: "sold-out" },
+                    { name: "Latecomer", price: 12, max: 100, sold: 0, code: "LATE", status: "coming-soon" }
                 ]
             }
         }
